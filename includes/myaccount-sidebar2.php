800979ee80365f1f8e72f245fa7a80f5b14b157a
duchuyvo0368 <voduchuy016688@gmail.com>
2024-06-07 15:23:33 +0700

    shopping

---
+++ includes/myaccount-sidebar2.php
@@ -0,0 +1,18 @@
+	<div class="col-md-4">
+					<!-- checkout-progress-sidebar -->
+<div class="checkout-progress-sidebar ">
+	<div class="panel-group">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+		    	<h4 class="unicase-checkout-title">Procceed to Payment</h4>
+		    </div>
+		    <div class="panel-body">
+				<ul class="nav nav-checkout-progress list-unstyled">
+					<li><a href="payment-method.php">Procceed to pay</a></li>
+					
+				</ul>		
+			</div>
+		</div>
+	</div>
+</div> 
+<!-- checkout-progress-sidebar -->				</div>
